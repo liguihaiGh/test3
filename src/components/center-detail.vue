356970ed377aa4b14b711fc29d8bb091649d7c17
Lgh <602460672@qq.com>
2018-04-23 17:21:29 +0800

    我的txt3

---
+++ src/components/center-detail.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="center-detail">
+    <div class="movie-main">
+      <p>标题党</p>
+      <article>
+        {{msg}}
+      </article>
+    </div>
+    <footerCom></footerCom>
+  </div>
+</template>
+<script>
+  import footerCom from '@/components/footerCom'
+  export default{
+    name:'centerDetail',
+    data(){
+      return{
+        msg:''
+      }
+    },
+    components:{
+      footerCom
+    },
+    methods:{
+      getMovieDetail(){
+        let _this = this;
+        _this.$axios.get('https://easy-mock.com/mock/5ad85acd8ddd0450275c0dd4/myTest/movieDetail'+"?id="+_this.$route.query.id)
+        .then(function(res){
+          _this.msg = res.data.data.list.content;
+        })
+        .catch(function(res){
+          console.log(res);
+        });
+      }
+    },
+    create(){
+
+    },
+    mounted(){
+      this.getMovieDetail();
+    }
+  }
+</script>
+<style lang="less" scoped>
+  .movie-main{
+    padding:10px;
+    font-size: 20px;
+    p{
+      text-align: center;
+      font-size: 24px;
+      font-weight: bold;
+      padding:10px 0;
+    }
+  }
+</style>
---
+++ src/components/shopcar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="shopcar">
+    <p>购物车</p>
+    <footerCom></footerCom>
+  </div>
+</template>
+<script>
+  import footerCom from '@/components/footerCom'
+  export default{
+    data(){
+      return{
+        name:'shopcarMd'
+      }
+    },
+    components:{
+      footerCom
+    }
+  }
+</script>
